d2b9045f34a23e8fefd66419ccc65b752bb6f8e2
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-02-26 21:27:55 +0200

    added getProduct by Id (studied lambdas, optional and custom exceptions)

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -33,6 +36,24 @@ public class ProductService {
         product.setImageUrl(request.getImageUrl());
 
         return productRepository.save(product);
+    }
+
+    public Product getProduct(long id) {
+        LOGGER.info("Retrieving product {}", id);
+
+        // optional usage explained
+//        Optional<Product> productOptional = productRepository.findById(id);
+//
+//        if (productOptional.isPresent()) {
+//            return productOptional.get();
+//        } else {
+//            throw new ResourceNotFoundException("Product " + id + " not found.");
+//        }
+
+        return productRepository.findById(id)
+                // lambda expressions
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product " + id + " not found."));
     }
 
 }
