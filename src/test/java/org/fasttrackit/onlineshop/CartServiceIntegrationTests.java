f83bf993c0ed52c607a829b3344fea7cbe1f4a4b
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-03-25 19:23:33 +0200

    fixed lazy loading exceptions in cart service

--- src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
@@ -1,18 +1,25 @@
 package org.fasttrackit.onlineshop;
 
+import org.fasttrackit.onlineshop.domain.Cart;
 import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.CartService;
 import org.fasttrackit.onlineshop.steps.CustomerTestSteps;
 import org.fasttrackit.onlineshop.steps.ProductTestSteps;
 import org.fasttrackit.onlineshop.transfer.cart.AddProductsToCartRequest;
+import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.fasttrackit.onlineshop.transfer.cart.ProductInCartResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest
 public class CartServiceIntegrationTests {
@@ -36,5 +43,25 @@ public class CartServiceIntegrationTests {
         cartRequest.setProductIds(Collections.singletonList(product.getId()));
 
         cartService.addProductsToCart(cartRequest);
+
+        CartResponse cart = cartService.getCart(customer.getId());
+
+        assertThat(cart, notNullValue());
+        assertThat(cart.getId(), is(customer.getId()));
+
+        assertThat(cart.getProducts(), notNullValue());
+        assertThat(cart.getProducts(), hasSize(1));
+
+        Iterator<ProductInCartResponse> productIterator =
+                cart.getProducts().iterator();
+
+        assertThat(productIterator.hasNext(), is(true));
+
+        ProductInCartResponse nextProduct = productIterator.next();
+
+        assertThat(nextProduct, notNullValue());
+        assertThat(nextProduct.getId(), is(product.getId()));
+        assertThat(nextProduct.getName(), is(product.getName()));
+        assertThat(nextProduct.getPrice(), is(product.getPrice()));
     }
 }
